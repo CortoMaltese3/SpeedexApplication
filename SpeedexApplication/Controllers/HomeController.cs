417a8770f46a517764dd5dd5db9a04560c2a7a66
Giorgos Kalomalos <Giorgos.Kalomalos@Peoplecert.onmicrosoft.com>
2019-04-03 18:54:03 +0300

    Added mainmenu grid

--- SpeedexApplication/Controllers/HomeController.cs
+++ SpeedexApplication/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using SpeedexApplication.Application;
 using SpeedexApplication.Models;
+using SpeedexApplication.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,14 +17,53 @@ namespace SpeedexApplication.Controllers
         public ActionResult Index()
         {
 
-            return View();
+
+            //var viewModel = new MainMenuGrid();
+
+            IEnumerable<MainMenuGrid> mainMenuGrid = null;
+
+            mainMenuGrid = (from c in db.Customer
+                                join a in db.Area on c.AreaId equals a.Id
+                                join ci in db.City on a.CityId equals ci.Id
+                                select new MainMenuGrid
+                                {
+                                    FirstName = c.FirstName,                                    
+                                    LastName = c.LastName,
+                                    Email = c.Email,
+                                    Area = a.AreaName,
+                                    PostalCode = a.PostCode,
+                                    City = ci.Name,
+                                    Country = ci.Country
+                                });
+            //foreach (var item in mainMenuGrid)
+            //{
+            //    item.FirstName = viewModel.FirstName;
+
+            //}
+
+
+            //foreach (var item in mainMenuGrid)
+            //{
+            //    item = viewModel;
+            //}
+
+            //var mainMenu = PopulateMainMenuGrid();
+            //PopulateMainMenuGrid();
+            return View(mainMenuGrid);
         }
 
-        //private void PopulateMainMenuGrid()
-        //{
-        //    var mainMenuGrid = (from c in db.Customer 
+        private IEnumerable<Customer> PopulateMainMenuGrid()
+        {
+            string sqlString = "select c.FirstName as [First Name], c.LastName as [Last Name], c.Email," +
+                               " a.AreaName as [Area], a.PostCode as [Postal Code], ci.[Name] as City, " +
+                               "ci.Country from Customer c left join Area a on c.AreaId = a.Id " +
+                               "left join City ci on ci.Id = a.CityId";
+            var mainMenuGrid = db.Customer.SqlQuery(sqlString).ToList();
+            //var mainMenuGrid = db.Database.SqlQuery<MainMenuGrid>(sqlString);
 
 
-        //}
+
+            return mainMenuGrid;
+        }
     }
 }
---
+++ SpeedexApplication/ViewModel/MainMenuGrid.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using SpeedexApplication.Models;
+
+namespace SpeedexApplication.ViewModel
+{
+    public class MainMenuGrid : Entity
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Area { get; set; }
+
+        [Display(Name = "Postal Code")]
+        public int PostalCode { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+
+
+        //public MainMenuGrid(Customer customer, Area area, City city)
+        //{
+        //    FirstName = customer.FirstName;
+        //    LastName = customer.LastName;
+        //    Email = customer.Email;
+        //    Area = area.AreaName;
+        //    PostalCode = area.PostCode;
+        //    City = city.Name;
+        //    Country = city.Country;
+
+        //}
+
+        //public IEnumerable<Customer> Customers { get; set; }
+        //public IEnumerable<Area> Areas { get; set; }
+        //public IEnumerable<City> Cities { get; set; }
+
+    }
+}
